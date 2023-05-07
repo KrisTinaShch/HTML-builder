4ba07fb7d55a9db33b408bea9eeae1a9b917c12e
KrisShe <kris.shcheglova@bk.ru>
2023-05-07 11:31:57 +0300

    feat: text output to console

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,2 +1,9 @@
 const fs = require('fs');
 const path = require('path');
+
+const folder = path.join(__dirname, 'text.txt');
+const readStream = fs.createReadStream(folder, 'utf8');
+
+readStream.on('data', (text) => {
+  console.log(text);
+});
