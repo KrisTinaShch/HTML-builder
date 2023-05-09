078b353b25ecccec40e234ce32c3317e49309419
Kris Shcheglova <kris.shcheglova@bk.ru>
2023-05-09 20:59:11 +0300

    fix : add right code in 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,74 @@
+const fs = require('fs').promises;
+const path = require('path');
 
+async function copyDir() {
+    const initFolder = path.join(__dirname, 'files');
+
+    const newFolder = path.join(__dirname, 'files-copy');
+
+    try {
+        await fs.mkdir(newFolder);
+    } catch (err) {
+        if (err.code !== 'EEXIST') {
+            throw err;
+        }
+    }
+
+    const files = await fs.readdir(initFolder);
+
+    await Promise.all(files.map(async (file) => {
+
+        const initFolder = path.join(initFolder, file);
+        const newPath = path.join(newFolder, file);
+
+        const stat = await fs.stat(initFolder);
+
+        if (stat.isFile()) {
+
+            await fs.copyFile(initFolder, newPath);
+
+        } else if (stat.isDirectory()) {
+
+            await copyDirRecursive(initFolder, newPath);
+
+        }
+    }));
+}
+
+async function copyDirRecursive(source, target) {
+
+    try {
+
+        await fs.mkdir(target);
+
+    } catch (err) {
+
+        if (err.code !== 'EEXIST') {
+
+            throw err;
+
+        }
+    }
+
+
+    const files = await fs.readdir(source);
+
+
+    await Promise.all(files.map(async (file) => {
+        const initFolder = path.join(source, file);
+
+        const newPath = path.join(target, file);
+
+        const stat = await fs.stat(initFolder);
+        
+        if (stat.isFile()) {
+            await fs.copyFile(initFolder, newPath);
+        } else if (stat.isDirectory()) {
+            await copyDirRecursive(initFolder, newPath);
+        }
+    }));
+}
+
+copyDir()
+    .then(() => console.log('Папка скопирована!'))
+    .catch((err) => console.error(err));
